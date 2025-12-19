ca0d231d182718bbf44aa15da31f565321b52167
Ethan Hanks <ethanjhanks99@gmail.com>
2025-12-18 23:43:54 -0700

    checks args length

--- src/scaff.sh
+++ src/scaff.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 usage() {
-  echo "Usage: scaffold <action> <entity-type> [--type TYPE] " >&2
+  echo "Usage: scaffold <action> <entity_type> <name> [options] " >&2
   exit 1
 }
+
+if [[ $@ < 4 ]]; then
+  echo "Insufficient arguments provided!"
+  usage
+  exit 1
+fi
+
+action=$1
+entity_type=$2
+name=$3
+
+echo "action: $action"
+echo "entity-type: $entity_type"
+echo "name: $name"
